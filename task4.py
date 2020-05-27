9bff7b03294937175b927cc77ae8bae96faee857
Nikita Koznev <nikitakoznev@gmail.com>
2020-05-28 01:10:49 +0300

    Task4 done

--- task4.py
+++ task4.py
@@ -10,37 +10,35 @@ def input_matrix(m):
 
 
 def input_vector():
-    return list(map(int, input().split()))
+    return np.array(list(map(int, input().split())))
 
 
-def get_inverted(matrix_b, vector_x, position):
+def get_inverted_matrix(matrix_b, vector_x, position):
     vector_l = matrix_b.dot(vector_x.T)
+
     if vector_l[position] == 0:
         return None
 
     vector_l_cover = vector_l[position]
     vector_l[position] = -1
     vector_l *= -1 / vector_l_cover
+    matrix_b_new = matrix_b + vector_l.reshape(-1, 1) * matrix_b[position]
+    matrix_b_new[position] -= matrix_b[position]
 
-    matrix_b_new = np.eye(len(matrix_b), dtype=float)
-    matrix_b_new[:, position] = vector_l
-
-    return matrix_b_new.dot(matrix_b)
+    return matrix_b_new
 
 
-def main_stage_simplex_method(matrix_a, vector_b, vector_c, vector_x, vector_jb):
+def main_stage_simplex_method(matrix_a, vector_c, vector_x, vector_jb):
     m = len(matrix_a)
     n = len(matrix_a[0])
+
     if m == n:
         return vector_x, vector_jb, None
-    matrix_ab = matrix_a[:, vector_jb]
-    matrix_b = np.linalg.inv(matrix_ab)
 
+    matrix_b = np.linalg.inv(matrix_a[:, vector_jb])
     while True:
-        vector_jb_n = [i for i in range(n) if i not in vector_jb]
-
-        vector_u = vector_c[vector_jb].dot(matrix_b)
-        delta = vector_u.dot(matrix_a[:, vector_jb_n]) - vector_c[vector_jb_n]
+        delta = vector_c[vector_jb].dot(matrix_b).dot(matrix_a) - vector_c
+        delta[vector_jb] = math.inf
 
         checker = -1
         for i, el in enumerate(delta):
@@ -50,56 +48,58 @@ def main_stage_simplex_method(matrix_a, vector_b, vector_c, vector_x, vector_jb)
         if checker == -1:
             return vector_x, vector_jb, matrix_b
 
-        j0 = vector_jb_n[checker]
+        j0 = checker
 
         vector_z = matrix_b.dot(matrix_a[:, j0])
+
         if all([i <= 0 for i in vector_z]):
             return None, None, None
 
         theta = [vector_x[vector_jb[i]] / vector_z[i] if vector_z[i] > 0 else math.inf for i in range(m)]
+        theta0 = min(theta)
 
-        theta_0 = min(theta)
-        s = theta.index(theta_0)
+        s = theta.index(theta0)
         vector_jb[s] = j0
 
-        matrix_b = get_inverted(matrix_b, matrix_a[:, j0], s)
+        matrix_b = get_inverted_matrix(matrix_b, matrix_a[:, j0], s)
 
         if matrix_b is None:
             return None, None, None
 
         vector_x_new = np.zeros(n, dtype=float)
-        vector_x_new[vector_jb] = vector_x[vector_jb] - theta_0 * vector_z
-        vector_x_new[j0] = theta_0
+        vector_x_new[vector_jb] = vector_x[vector_jb] - theta0 * vector_z
+        vector_x_new[j0] = theta0
         vector_x = vector_x_new
 
 
-def first_and_second_step_simplex_method(matrix_a, vector_b, vector_c, m, n):
+def first_and_second_step_simplex_method(matrix_a, vector_b, vector_c):
+    m = len(matrix_a)
+    n = len(matrix_a[0])
+
     for i in range(m):
         if vector_b[i] < 0:
             vector_b[i] *= -1
             matrix_a[i] *= -1
 
-    vector_jb = [i for i in range(n, n + m)]
+    vector_jb_cover = [i for i in range(n, n + m)]
 
-    matrix = np.concatenate((matrix_a, np.eye(m)), axis=1)
-
-    vector_c_1 = np.concatenate((np.zeros(n), np.ones(m)))
-    vector_x_start = np.concatenate((np.zeros(n), np.copy(vector_b)))
+    vector_c_cover = np.concatenate((np.zeros(n), np.ones(m)))
+    vector_x = np.concatenate((np.zeros(n), np.copy(vector_b)))
 
     vector_x, vector_jb, matrix_b = main_stage_simplex_method(
-        matrix, vector_b, -vector_c_1, vector_x_start, vector_jb)
+        np.concatenate((matrix_a, np.eye(m)), axis=1), -vector_c_cover, vector_x, vector_jb_cover)
 
     if vector_x is None:
         return None
 
-    vector_x_0 = vector_x[:n]
-    vector_x_u = vector_x[n:]
+    vector_x_0 = vector_x[:n] 
 
-    if any(vector_x_0 < 0) or any(vector_x_u != 0):
+    if any(vector_x_0 < 0) or any(vector_x[n:] != 0):
         return ()
 
     while True:
         vector_jb_n = [i for i in range(len(matrix_a[0])) if i not in vector_jb]
+
         checker = -1
         for i, el in enumerate(vector_jb):
             if el >= n:
@@ -107,30 +107,31 @@ def first_and_second_step_simplex_method(matrix_a, vector_b, vector_c, m, n):
                 break
 
         if checker == -1:
-            vector_x_new, vector_jb, matrix_b = main_stage_simplex_method(
-                matrix_a, vector_b, vector_c, vector_x_0, vector_jb)
-            return vector_x_new
+            vector_x, vector_jb, matrix_b,  = main_stage_simplex_method(
+                matrix_a, vector_c, vector_x_0, vector_jb)
+            return vector_x
 
-        jk = vector_jb[checker]
-        i0 = jk - n
+        el_j_k = vector_jb[checker]
+        i0 = el_j_k - n
 
-        e = np.zeros(len(matrix_a))
-        e[i0] = 1
-        alphas = e.dot(matrix_b).dot(matrix_a[:, vector_jb_n])
+        vector_e = np.zeros(m)
+        vector_e[i0] = 1
 
-        if all(alphas == 0):
+        vector_of_alphas = vector_e.dot(matrix_b).dot(matrix_a[:, vector_jb_n])
+
+        if all(vector_of_alphas == 0):
             matrix_a = np.delete(matrix_a, i0, 0)
             matrix_b = np.delete(matrix_b, i0, 1)
             matrix_b = np.delete(matrix_b, checker, 0)
-            vector_jb.remove(jk)
+            vector_jb.remove(el_j_k)
 
-            for i, el in enumerate(vector_jb):
-                if el > jk:
-                    el -= 1
+            for i in range(len(vector_jb)):
+                if vector_jb[i] > el_j_k:
+                    vector_jb[i] -= 1
         else:
             i = 0
-            for alpha in alphas:
-                if alpha != 0:
+            for a in vector_of_alphas:
+                if a != 0:
                     break
                 i += 1
 
@@ -141,8 +142,10 @@ def first_and_second_step_simplex_method(matrix_a, vector_b, vector_c, m, n):
 def simplex():
     m, n = map(int, input().split())
     matrix_a = input_matrix(m)
-    vector_b, vector_c = input_vector(), input_vector()
-    vector_x = first_and_second_step_simplex_method(matrix_a, vector_b, np.array(vector_c), m, n)
+    vector_b = input_vector()
+    vector_c = input_vector()
+
+    vector_x = first_and_second_step_simplex_method(matrix_a, vector_b, vector_c)
 
     if vector_x is None:
         return "Unbounded"
